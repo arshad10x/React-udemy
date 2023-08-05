d759d25bc143531f75b95b5a6f18aef2b3d402ee
Arshad <ahmed.a0611@gmail.com>
2023-08-05 18:08:26 +0530

    template literal

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import './App.css';
 
 function App() {
+  //Styling in react
+  const style ={
+    color:'blue',
+    backgroundColor:'black'
+  }
+  //Template Literal
+  const var1='Expert';
+  const var2 = 'Developer'
+
   return (
     <div className="App">
+      {/* Inline Styleing */}
+      <h1 style={style}>We are {var1} {var2}</h1>
+
     </div>
   );
 }
