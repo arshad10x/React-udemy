ecb054a1d90aa72c656fe2edf996f5f25c6d8044
Arshad <ahmed.a0611@gmail.com>
2023-08-05 18:10:15 +0530

    template literal

--- src/App.js
+++ src/App.js
@@ -5,14 +5,13 @@ function App() {
     color:'blue',
     backgroundColor:'black'
   }
-  //Template Literal
   const var1='Expert';
   const var2 = 'Developer'
 
   return (
     <div className="App">
-      {/* Inline Styleing */}
-      <h1 style={style}>We are {var1} {var2}</h1>
+      {/* Template Literals*/}
+      <h1 style={style}>{`We are ${var1} ${var2}`}</h1>
 
     </div>
   );
