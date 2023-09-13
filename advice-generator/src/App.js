5e7952d43de9e39eb0a2c619d5aa74fa1c6611d9
arshad10x <arshadahmed10x@gmail.com>
2023-09-13 18:45:02 +0530

    done

--- advice-generator/src/App.js
+++ advice-generator/src/App.js
@@ -1,24 +1,29 @@
-import './App.css';
-
-function App() {
+import {useState,useEffect} from 'react';
+export default function App(){
+  
+  const [advice, setAdvice] = useState("");
+  const [count,setCount]=useState(0);
+  async function getAdvice(){
+    const res = await fetch('https://api.adviceslip.com/advice');
+    const data = await res.json();
+    setAdvice(data.slip.advice);
+    setCount(e=>e+1);
+  }
+  useEffect(function(){
+    getAdvice();
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+    <h1>{advice}</h1>
+    <button onClick={getAdvice}>Get Advise</button>
+      <Message count={count}/>
+  </>
   );
 }
 
-export default App;
+function Message(props){
+  return (
+   <p>
+    Great!! you have read <strong>{props.count}</strong>peices of advice.
+  </p>)
+}
