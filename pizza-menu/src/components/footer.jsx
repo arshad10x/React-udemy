d9143d3b6a924a500b763a689bbefffc9a1f4025
arshad10x <arshadahmed10x@gmail.com>
2023-10-17 14:40:38 +0530

    err resolve

--- pizza-menu/src/components/footer.jsx
+++ pizza-menu/src/components/footer.jsx
@@ -10,10 +10,9 @@ function Footer(){
   // if(hour >=openHours && hour <= closeHourse) alert(`We're currently open`)
   // else alert('Sorry we are closed')
     return (
-      <div>
-        <h2>We're currently open</h2>
-        <h3>{new Date().toLocaleDateString()}</h3>.
-       
+      <div className="footer">
+        We're currently open
+        {new Date().toLocaleDateString()}.
       </div>
     )
   }
