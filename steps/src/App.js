baa03f52d5b9931cafd6ee13ce5095dab5309365
arshad10x <arshadahmed10x@gmail.com>
2023-10-18 18:50:48 +0530

    event

--- steps/src/App.js
+++ steps/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+const message = ["Learn React *", "Build Projects **", "Apply for Jobs"];
+
+export default function App() {
+  const btnStyle={ backgroundColor: "#7950f2", color: "#fff" }
+  const step = 1;
+
+  function handlePrevious(){
+    alert('previous')
+
+  }
+
+  function handleNext(){
+    alert('Next')
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="steps">
+      <div className="numbers">
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+      </div>
+      <p className="message">
+        Step {step} : {message[step - 1]}
+      </p>
+      <div className="buttons">
+        <button style={btnStyle}
+        onClick={handlePrevious}>
+          Previous
+        </button>
+        <button style={btnStyle}
+        onClick={handleNext}
+        >Next</button>
+      </div>
     </div>
   );
 }
-
-export default App;
