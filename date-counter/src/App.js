115906b1e1016dd3cec1091987925824701d79ef
arshad10x <arshadahmed10x@gmail.com>
2023-11-01 11:35:19 +0530

    done

--- date-counter/src/App.js
+++ date-counter/src/App.js
@@ -1,26 +1,42 @@
-import { useState } from 'react';
-import './App.css';
-
+import { useState } from "react";
+import "./App.css";
 
 export default function App() {
   return (
     <div>
-    <Counter/>
+      <Counter />
     </div>
   );
 }
 
-function Counter(){
-  const [counter,setCounter]=useState(0)
-
-  return(
+function Counter() {
+  const [counter, setCounter] = useState(0);
+  const [step,setStep]=useState(1)
+  const date = new Date("Oct 30 2023");
+  date.setDate(date.getDate() + counter);
+  return (
     <div>
-      <button onClick={()=>setCounter(e=>e-1)}>-</button>
-      <span>Count: {counter}</span>
-      <button onClick={()=>setCounter(e=>e+1)}>+</button>
-      <span>Count: {counter}</span>
-      
+      <div>
+        <button onClick={() => setStep((e) => e - 1)}>-</button>
+        <span>Step: {step}</span>
+        <button onClick={() => setStep((e) => e + 1)}>+</button>
+      </div>
 
+      <div>
+        <button onClick={() => setCounter((e) => e - step)}>-</button>
+        <span>Count: {counter}</span>
+        <button onClick={() => setCounter((e) => e + step)}>+</button>
+      </div>
+      <p>   
+        <span>
+          {counter === 0
+            ? "Today  is"
+            : counter > 0
+            ? `${counter} days from today is `
+            : `${Math.abs(counter)} days ago was`}
+        </span>
+        <span>{date.toDateString()}</span>
+      </p>
     </div>
-  )
-} 
+  );
+}
